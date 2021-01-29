8393d2d95e0b4cd03e91570ccfc5bca28ec60bdf
ua7605 <vincent.charpentier@student.uantwerpen.be>
2021-01-29 17:19:25 +0100

    added finalSection
    This in stll in development

---
+++ src/main/java/optionals/_Optionals.java
@@ -0,0 +1,11 @@
+package optionals;
+
+import java.util.Optional;
+
+public class _Optionals {
+
+    public static void main(String[] args) {
+
+        Optional<Object> empty = Optional.empty();
+    }
+}
