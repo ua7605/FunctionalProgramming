957a9b65b220fe9af575e8065587e7ec7f18b011
ua7605 <vincent.charpentier@student.uantwerpen.be>
2021-01-29 11:21:21 +0100

    Funtions completed

--- src/main/java/Functionalinterface/_Function.java
+++ src/main/java/Functionalinterface/_Function.java
@@ -13,14 +13,23 @@ public class _Function {
         int crementFunction = incrementFunctionbyOne.apply(1);
         System.out.println(crementFunction);
 
-    }
 
+        Integer twoFunctions = incrementFunctionbyOneANDTHENmultipleby10.apply(2);
+        System.out.println(twoFunctions);
+
+    }                   //  INPUT , OUTPUT
     static public Function<Integer, Integer> incrementFunctionbyOne =
-            integer -> integer++;
+            integer -> integer + 1;
+
+
+    static public Function<Integer, Integer> multipleby10 =
+            integer -> integer * 10;
+
+    static public Function<Integer, Integer> incrementFunctionbyOneANDTHENmultipleby10 =
+            incrementFunctionbyOne.andThen(multipleby10);
 
     static public int increment(int number)
     {
         return number++;
-
     }
 }
